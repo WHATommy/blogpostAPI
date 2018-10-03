16b619a335de926c9c65a729c7bbdeb53f6d077b
unknown <tranman100598@gmail.com>
2018-10-03 15:05:49 -0700

    updated server

--- server.js
+++ server.js
@@ -14,3 +14,43 @@ app.use('/blog', blogRouter);
 app.listen(process.env.PORT || 8080, () => {
     console.log(`Your app is listening on port ${process.env.PORT || 8080}`)
 })
+
+let server;
+
+function runServer(databaseUrl, port = PORT) {
+    return new Promise((resolve, reject) => {
+        mongoose.connect(databaseUrl, err => {
+            if (err) {
+                return reject(err);
+            }
+            server = app.listen(port, () => {
+                console.log(`Your app is listening on port ${port}`);
+                resolve();
+            })
+            .on('error', err => {
+                mongoose.disconnect();
+                reject(err);
+            });
+        });
+    });
+}
+
+function closeServer() {
+    return mongoose.disconnect().then(() => {
+        return new Promise((resolve, reject) => {
+            console.log('Closing server');
+            server.close(err => {
+                if (err) {
+                   return reject(err);
+                }
+                resolve();
+            });
+        });
+    });
+}
+
+if (require.main === module) {
+    runServer(databaseUrl).catach(err => console.error(err));
+}
+
+module.export = { runServer, app, closeServer }

a4b4db7be409ad5844fa5f88e914a4be2c9a14bc
unknown <tranman100598@gmail.com>
2018-10-03 18:04:23 -0700

    Added schema

--- models.js
+++ models.js
@@ -66,5 +66,44 @@ function createBlogPostsModel() {
   return storage;
 }
 
+let authorSchema = mongoose.Schema({
+  firstName: 'string',
+  lastName: 'string',
+  userName: {
+    type: 'string',
+    unique: true
+  }
+});
 
-module.exports = {BlogPosts: createBlogPostsModel()};
+let commentSchema = mongoose.Schema({ content: 'string'});
+
+let blogPostSchema = mongoose.Schema({
+  title: 'string',
+  content: 'string',
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'Author'},
+  comment: [commentSchema]
+});
+
+blogPostSchema.pre('find', function(next) {
+  this.populate('author');
+  next();
+});
+
+blogPostSchema.virtual('authorName').get(function() {
+  return `${this.author.firstName} ${this.author.lastName}`.trim();
+});
+
+blogPostSchema.methods.serialize = function() {
+  return {
+    id: tihs._id,
+    author: this.authorName,
+    content: this.content,
+    title: this.title,
+    comments: tihs.comments
+  };
+};
+
+let Author = mongoose.model('Author', authorSchema);
+let BlogPost = mongoose.mode('BlogPost', blogPostSchema);
+
+module.exports = {BlogPosts: createBlogPostsModel(), Author, BlogPost};
